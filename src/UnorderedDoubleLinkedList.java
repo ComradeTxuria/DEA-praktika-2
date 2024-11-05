a089e39745c3732667e28f508bea02a9af6ae845
CH3MBER <166596453+CH3MBER@users.noreply.github.com>
2024-11-05 09:01:06 +0100

    Update UnorderedDoubleLinkedList.java

--- src/UnorderedDoubleLinkedList.java
+++ src/UnorderedDoubleLinkedList.java
@@ -2,20 +2,53 @@ package listasSimples;
 
 public class UnorderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements UnorderedListADT<T> {
 	
-	public void addToFront(T elem) {
-	// hasieran gehitu
-		// KODEA OSATU ETA KOSTUA KALKULATU
+    @Override
+    public void addToFront(T elem) {
+        Node<T> berria = new Node<>(elem);
+        berria.next = first;              
+        berria.prev = null; 
+        if (isEmpty()) {
+            last = berria;
+        } else {
+            first.prev = berria;
+        }
+        first = berria;
+        count++;
+    } //KOSTUA addToFront(T elem): O(1)
 
-	}
+    @Override
+    public void addToRear(T elem) {
+        Node<T> berria = new Node<>(elem);
+        berria.next = null;              
+        berria.prev = last; 
+        if (isEmpty()) {
+            first = berria;
+        } else {
+            last.next = berria;
+        }
+        last = berria;
+        count++;
+    } //KOSTUA addToRear(T elem): O(1)
 
-	public void addToRear(T elem) {
-	// bukaeran gehitu
-		// KODEA OSATU ETA KOSTUA KALKULATU
+    @Override
+    public void addAfter(T elem, T target) {
+        Node<T> unekoa = first;
+        while (unekoa != null && !unekoa.data.equals(target)) {
+            unekoa = unekoa.next;
+        }
 
-	}
-	
-	public void addAfter(T elem, T target) {
-		// KODEA OSATU ETA KOSTUA KALKULATU (AUKERAZKOA)
-	}
+        if (unekoa != null) {
+            Node<T> berria = new Node<>(elem);
+            berria.next = unekoa.next;              
+            berria.prev = unekoa; 
+            if (unekoa.next != null) {
+                unekoa.next.prev = berria;
+            } else {
+                last = berria;
+            }
+            unekoa.next = berria;
+            count++;
+        }
+    } //KOSTUA addAfter(T elem, T target): O(n)
 
 }
